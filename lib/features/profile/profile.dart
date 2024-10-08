2d91004e0dde2fd02e67feeee4b153bf29a3dd0a
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-10-08 20:24:00 +0200

    made many Experiments with "ProfilePage"

--- lib/features/profile/profile.dart
+++ lib/features/profile/profile.dart
@@ -15,20 +15,56 @@ class MyProfile extends StatelessWidget {
       home: Scaffold(
         // den gesamten Hintergrund einfärben:
         backgroundColor: primeColor,
-        // appBar: AppBar(
-        //   title: const Text('Mein Profil'),
-        //   foregroundColor: Colors.white, // Schriftfarbe
-        //   backgroundColor:
-        //       const Color.fromARGB(255, 4, 85, 151), // dunkles Blau
-        //   shadowColor: Colors.black87, // Schattten unter der AppBar
-        //   elevation: 10, // Höhe des Schattens unter der AppBar
-        // ),
-        body: SafeArea(
-          child: Text('MyProfile'),
+// ----------------------------------------------------------------------------------------------
+// AppBar
+// ----------------------------------------------------------------------------------------------
+//         appBar: AppBar(
+//           title: const Text('Mein Profil'),
+//           foregroundColor: Colors.white, // Schriftfarbe
+//           backgroundColor:
+//               const Color.fromARGB(255, 4, 85, 151), // dunkles Blau
+//           shadowColor: Colors.black87, // Schattten unter der AppBar
+//           elevation: 10, // Höhe des Schattens unter der AppBar
+//         ),
+// ----------------------------------------------------------------------------------------------
+        body: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+
+          // Hier soll mein Profilbild in einem runden Kreis angezeigt werden
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              CircleAvatar(
+                backgroundImage: AssetImage("assets/images/kaya.jpeg"),
+                radius: 100,
+              ),
+              Column(
+                children: [
+                  SizedBox(height: 20),
+                  Text(
+                    "Kaya Müller",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  Text(
+                    "Fotograf",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+// ----------------------------------------------------------------------------------------------
+                  Text(
+                    "Mein Name ist Kaya Müller, ein Fotograf im Alter von 32 Jahren, der die Welt durch die Linse entdeckt. Mit einem Hintergrund in der visuellen Kunst und jahrelanger Erfahrung in der Fotografie habe ich einen Blick für die Schönheit im Alltäglichen entwickelt. Meine Spezialität liegt in der Sport- und Naturfotografie, wo ich das Spiel von Licht und Schatten einfange, um emotionale und aussagekräftige Bilder zu schaffen.\n\nIch glaube daran, dass jeder Moment einzigartig ist und seine eigene Geschichte erzählt. In meinen Arbeiten strebe ich danach, diese Geschichten visuell zu interpretieren und sie durch kreative Kompositionen und Nuancen zum Leben zu erwecken. Meine fotografische Reise hat mich durch verschiedene Länder geführt, wo ich die Vielfalt der Kulturen und Landschaften festhalte, immer auf der Suche nach neuen Perspektiven und Herausforderungen.\n\nMein Anspruch ist es, mit meinen Bildern nicht nur zu dokumentieren, sondern auch zu inspirieren und zu berühren. Jeder Auftrag und jedes Projekt ist für mich eine Möglichkeit, meine Leidenschaft und mein Können zu zeigen und eine Verbindung zwischen dem Betrachter und dem Bild herzustellen.",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-// // ----------------------------------------------------------------------------------------------
-// // Standard NavigationBar
-// // ----------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------
+// Standard NavigationBar
+// ----------------------------------------------------------------------------------------------
 //         bottomNavigationBar: NavigationBar(
 //           backgroundColor:
 //               Colors.amber, //const Color.fromARGB(255, 4, 85, 151),
@@ -56,7 +92,7 @@ class MyProfile extends StatelessWidget {
 //             ),
 //           ],
 //         ),
-// // ----------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------
       ),
     );
   }
