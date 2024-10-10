06ed1075b8f649cf78010f36f34402bd973dba23
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-10-10 10:31:03 +0200

    renamed "favorites" to "favorites_screen"

---
+++ lib/features/gallery/screens/favorites_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Favorites extends StatelessWidget {
+  const Favorites({super.key, required image});
+  static const primeColor = Color.fromRGBO(200, 210, 255, 1); // hellblau
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primeColor,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 235, 182, 210),
+        title: const Text(
+          "Meine Favoriten-Bilder \n--> (coming soon!) <--",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      ),
+
+      // Hier sollen die Favoriten-Bilder in einem Carusell angezeigt werden // todo!
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(40),
+          child: Image(
+            image: AssetImage("assets/images/ski.jpeg"),
+          ),
+        ),
+      ),
+    );
+  }
+}
