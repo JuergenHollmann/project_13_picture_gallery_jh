14cef2f44ad2ee09648093a7d030396717dbcfb7
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-10-10 15:27:45 +0200

    redesign text in "textAlign: TextAlign.justify"

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Individual Picture Gallery project.
 Es wurde eine Bildergalerie mit vorgegebenen Bildern und Beschreibungstext innerhalb von 2 Tagen programmiert.
 Bei der Farbgestaltung und der Aufteilung gab es eigenen Gestaltungsspielraum.
 #
-![1](<Mittel (Bild 1).png>) ![2](<Mittel (Bild 2).png>) ![3](<Mittel (Bild 3).png>) [x](README.md)
+![1](<Mittel (Bild 1).png>) ![2](<Mittel (Bild 2).png>) ![3](<Mittel (Bild 3).png>)
--- lib/features/profile/my_profile.dart
+++ lib/features/profile/my_profile.dart
@@ -59,8 +59,12 @@ class MyProfile extends StatelessWidget {
                     child: Padding(
                       padding: EdgeInsets.symmetric(horizontal: 24),
                       child: Text(
-                        "Mein Name ist Kaya Müller, ein Fotograf im Alter von 32 Jahren, der die Welt durch die Linse entdeckt. Mit einem Hintergrund in der visuellen Kunst und jahrelanger Erfahrung in der Fotografie habe ich einen Blick für die Schönheit im Alltäglichen entwickelt. Meine Spezialität liegt in der Sport- und Naturfotografie, wo ich das Spiel von Licht und Schatten einfange, um emotionale und aussagekräftige Bilder zu schaffen.\n\nIch glaube daran, dass jeder Moment einzigartig ist und seine eigene Geschichte erzählt. In meinen Arbeiten strebe ich danach, diese Geschichten visuell zu interpretieren und sie durch kreative Kompositionen und Nuancen zum Leben zu erwecken. Meine fotografische Reise hat mich durch verschiedene Länder geführt, wo ich die Vielfalt der Kulturen und Landschaften festhalte, immer auf der Suche nach neuen Perspektiven und Herausforderungen.\n\nMein Anspruch ist es, mit meinen Bildern nicht nur zu dokumentieren, sondern auch zu inspirieren und zu berühren. Jeder Auftrag und jedes Projekt ist für mich eine Möglichkeit, meine Leidenschaft und mein Können zu zeigen und eine Verbindung zwischen dem Betrachter und dem Bild herzustellen.",
-                        style: TextStyle(fontSize: 16),
+                        "Mein Name ist Kaya Müller, ein Fotograf im Alter von 32 Jahren, der die Welt durch die Linse entdeckt. Mit einem Hintergrund in der visuellen Kunst und jahrelanger Erfahrung in der Foto-grafie habe ich einen Blick für die Schönheit im Alltäglichen entwickelt. Meine Spezialität liegt in der Sport- und Naturfotografie, wo ich das Spiel von Licht und Schatten einfange, um emotionale und aussagekräftige Bilder zu schaffen.\n\nIch glaube daran, dass jeder Moment einzigartig ist und seine eigene Geschichte erzählt.\n\nIn meinen Arbeiten strebe ich danach, diese Geschichten visuell zu interpretieren und sie durch kreative Kompositionen und Nuancen zum Leben zu erwecken. Meine fotografische Reise hat mich durch verschiedene Länder geführt, wo ich die Vielfalt der Kulturen und Landschaften festhalte, immer auf der Suche nach neuen Perspektiven und Herausforderungen.\n\nMein Anspruch ist es, mit meinen Bildern nicht nur zu dokumentieren, sondern auch zu inspi-rieren und zu berühren. Jeder Auftrag und jedes Projekt ist für mich eine Möglichkeit, meine Leidenschaft und mein Können zu zeigen und eine Verbindung zwischen dem Betrachter und dem Bild herzustellen.",
+                        textAlign: TextAlign
+                            .justify, // richtet den Text im Blocksatz aus
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
                       ),
                     ),
                   ),
